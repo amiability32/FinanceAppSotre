02e645230b1a7b6b0cbb954252ed5bd64c9ef3fa
sally <sally@sonnim.kr>
2017-04-13 21:19:15 +0900

    App detail view 에 scrollView 추가

--- FinanceAppStore/Model/AppDetailVO.swift
+++ FinanceAppStore/Model/AppDetailVO.swift
@@ -9,26 +9,29 @@
 import SwiftyJSON
 
 fileprivate struct JsonNames {
-    
     static let ID = "trackId"
     static let NAME = "trackName"
+    static let ICON_URL = "artworkUrl60"
     static let SCREENSHOT_URLS = "screenshotUrls"
     static let DESCRIPTION = "description"
+    static let RATING = "averageUserRatingForCurrentVersion"
 }
-
 
 class AppDetailVO {
     let id: Int
     let name: String
+    let iconUrl: String
     let screenShotUrl: [String]
     let description: String
-    
+    let rating: Float
     
     init(_ json: JSON) {
         id = json[JsonNames.ID].intValue
         name = json[JsonNames.NAME].stringValue
+        iconUrl = json[JsonNames.ICON_URL].stringValue
         screenShotUrl = json[JsonNames.SCREENSHOT_URLS].arrayObject as! [String]
         description = json[JsonNames.DESCRIPTION].stringValue
+        rating = json[JsonNames.RATING].floatValue
     }
     
 }
--- FinanceAppStore/View/AppDetailViewController.swift
+++ FinanceAppStore/View/AppDetailViewController.swift
@@ -11,13 +11,49 @@ import UIKit
 class AppDetailViewController: UIViewController {
 
     var appId: String?
+    fileprivate var appDetail: AppDetailVO?
     var viewModel: AppDetailViewModel?
+    
+    @IBOutlet var titleView: UIView!
+    @IBOutlet var screenShotView: UIView!
+    @IBOutlet var descriptionView: UIView!
+    
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let appId = appId {
             viewModel = AppDetailViewModel(appId: appId)
+            viewModel?.delegate = self
         }
+        
+    }
+}
+
+extension AppDetailViewController: AppDetailViewModelDelegate {
+    
+    func reloadView() {
+        appDetail = viewModel?.appDetail
+        
+        setTitleView()
+        setScreenShotView()
+        setDescriptionView()
+    }
+    
+    private func setTitleView() {
+        guard let appDetail = appDetail else { return }
+        
+        iconImageView.af_setImage(withURL: URL(string: appDetail.iconUrl)!)
+        nameLabel.text = appDetail.name
+    }
+    
+    private func setScreenShotView() {
+        guard let appDetail = appDetail else { return }
+    }
+    
+    private func setDescriptionView() {
+        guard let appDetail = appDetail else { return }
     }
 }
